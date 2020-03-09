cfa1a17d5ef744e8d840f6b02b15275a9ec249a1
钟健鑫 <zhongjianxin@zjx.local>
2020-03-09 23:17:27 +0800

    [Jason]refactor: removed some unused code and use new direction in marsrover'

--- pkg/marsrover/directions.go
+++ pkg/marsrover/directions.go
@@ -1,77 +1,47 @@
 package marsrover
 
-type Direction struct {
-	directionDisplay string
-}
-
-type DirectionType int
+type Direction int
 
 const (
-	N DirectionType = iota
+	N Direction = iota
 	E
 	S
 	W
 )
 
+var diractionMap = map[string]Direction{
+	"N": N,
+	"E": E,
+	"S": S,
+	"W": W,
+}
+
 func NewDirection(direction string) Direction {
-	return Direction{direction}
+	return Direction(valueOf(direction))
 }
 
-func valueOf(m *Direction) DirectionType {
-	return map[string]DirectionType{
-		"N": N,
-		"E": E,
-		"S": S,
-		"W": W,
-	}[m.directionDisplay]
+func valueOf(m string) Direction {
+	return diractionMap[m]
 }
 
-func (t DirectionType) string() string {
+func (t Direction) string() string {
 	return [...]string{"N", "E", "S", "W"}[t]
 }
 
-func (direction Direction) string() string {
-	return direction.directionDisplay
-}
-
 func (m *Direction) turnRight() {
-	directionType := valueOf(m)
-	newDirection := directionType + 1
+	//directionType := valueOf(m)
+	newDirection := *m + 1
 	if newDirection > 3 {
 		newDirection = 0
 	}
-	m.directionDisplay = newDirection.string()
+	*m = newDirection
 }
 
 func (m *Direction) turnLeft() {
-	directionType := valueOf(m)
-	newDirection := directionType - 1
+	//directionType := valueOf(m)
+	newDirection := *m - 1
 	if newDirection < 0 {
 		newDirection = 3
 	}
-	m.directionDisplay = newDirection.string()
-}
-
-func (m *Direction) turnRight2() {
-	if m.directionDisplay == "E" {
-		m.directionDisplay = "S"
-	} else if m.directionDisplay == "S" {
-		m.directionDisplay = "W"
-	} else if m.directionDisplay == "W" {
-		m.directionDisplay = "N"
-	} else if m.directionDisplay == "N" {
-		m.directionDisplay = "E"
-	}
-}
-
-func (m *Direction) turnLeft2() {
-	if m.directionDisplay == "E" {
-		m.directionDisplay = "N"
-	} else if m.directionDisplay == "N" {
-		m.directionDisplay = "W"
-	} else if m.directionDisplay == "W" {
-		m.directionDisplay = "S"
-	} else if m.directionDisplay == "S" {
-		m.directionDisplay = "E"
-	}
+	*m = newDirection
 }
