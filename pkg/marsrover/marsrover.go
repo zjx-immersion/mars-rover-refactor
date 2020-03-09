fdbc2e24c73bc645f1f08a264c71ffdbea6b5b96
钟健鑫 <zhongjianxin@zjx.local>
2020-03-09 17:59:02 +0800

    [Jason]refractor: removed the unused code and comments

--- pkg/marsrover/marsrover.go
+++ pkg/marsrover/marsrover.go
@@ -4,19 +4,9 @@ const forwardCommand = 'M'
 const leftCommand = 'L'
 const rightCommand = 'R'
 
-type MarsRover struct {
-	locationPointOfX int
-	locationPointOfY int
-	direction        string
-}
-
 type MarsRoverNew struct {
 	coordinate Coordinate
 	direction  string
-}
-
-func initLocation2(initLocationPointOfX, initLocationPointOfY int, initDirection string) MarsRover {
-	return MarsRover{initLocationPointOfX, initLocationPointOfY, initDirection}
 }
 
 func initLocation(initLocationPointOfX, initLocationPointOfY int, initDirection string) MarsRoverNew {
@@ -24,19 +14,6 @@ func initLocation(initLocationPointOfX, initLocationPointOfY int, initDirection 
 		Coordinate{initLocationPointOfX, initLocationPointOfY},
 		initDirection}
 }
-
-//func (m *MarsRover) sendCommand(commands []byte) (int, int, string) {
-//	for _, cmd := range commands {
-//		if cmd == byte(forwardCommand) {
-//			m.moveForward()
-//		} else if cmd == byte(leftCommand) {
-//			m.turnLeft()
-//		} else if cmd == byte(rightCommand) {
-//			m.turnRight()
-//		}
-//	}
-//	return m.locationPointOfX, m.locationPointOfY, m.direction
-//}
 
 func (m *MarsRoverNew) sendCommand(commands []byte) (int, int, string) {
 	for _, cmd := range commands {
@@ -72,18 +49,6 @@ func (m *MarsRoverNew) turnLeft() {
 		m.direction = "S"
 	} else if m.direction == "S" {
 		m.direction = "E"
-	}
-}
-
-func (m *MarsRover) moveForward() {
-	if m.direction == "E" {
-		m.locationPointOfX += 1
-	} else if m.direction == "W" {
-		m.locationPointOfX -= 1
-	} else if m.direction == "N" {
-		m.locationPointOfY += 1
-	} else if m.direction == "S" {
-		m.locationPointOfY -= 1
 	}
 }
 
