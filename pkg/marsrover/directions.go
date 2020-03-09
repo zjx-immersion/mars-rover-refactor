44925fc59be4c4e40e22466361b6024dcebfe031
钟健鑫 <zhongjianxin@zjx.local>
2020-03-09 20:19:04 +0800

    [Jason]refactor: new direction for turn left and right ready for replace the old logic

--- pkg/marsrover/directions.go
+++ pkg/marsrover/directions.go
@@ -1,12 +1,37 @@
 package marsrover
 
-type Direction string
+type Direction struct {
+	direction string
+}
 
 func NewDirection(direction string) Direction {
-	return Direction(direction)
+	return Direction{direction}
 }
 
 func (direction Direction) string() string {
-	return string(direction)
+	return direction.direction
 }
 
+func (m *Direction) turnRight() {
+	if m.direction == "E" {
+		m.direction = "S"
+	} else if m.direction == "S" {
+		m.direction = "W"
+	} else if m.direction == "W" {
+		m.direction = "N"
+	} else if m.direction == "N" {
+		m.direction = "E"
+	}
+}
+
+func (m *Direction) turnLeft() {
+	if m.direction == "E" {
+		m.direction = "N"
+	} else if m.direction == "N" {
+		m.direction = "W"
+	} else if m.direction == "W" {
+		m.direction = "S"
+	} else if m.direction == "S" {
+		m.direction = "E"
+	}
+}
--- pkg/marsrover/marsrover.go
+++ pkg/marsrover/marsrover.go
@@ -6,13 +6,37 @@ const rightCommand = 'R'
 
 type MarsRoverNew struct {
 	coordinate Coordinate
+	direction  string
+}
+
+type MarsRover struct {
+	coordinate Coordinate
 	direction  Direction
+}
+
+func initLocation2(initLocationPointOfX, initLocationPointOfY int, initDirection string) MarsRover {
+	return MarsRover{
+		Coordinate{initLocationPointOfX, initLocationPointOfY},
+		NewDirection(initDirection)}
 }
 
 func initLocation(initLocationPointOfX, initLocationPointOfY int, initDirection string) MarsRoverNew {
 	return MarsRoverNew{
 		Coordinate{initLocationPointOfX, initLocationPointOfY},
-		NewDirection(initDirection)}
+		initDirection}
+}
+
+func (m *MarsRover) sendCommand(commands []byte) (int, int, string) {
+	for _, cmd := range commands {
+		if cmd == byte(forwardCommand) {
+			m.moveForward()
+		} else if cmd == byte(leftCommand) {
+			m.direction.turnLeft()
+		} else if cmd == byte(rightCommand) {
+			m.direction.turnRight()
+		}
+	}
+	return m.coordinate.locationPointOfX, m.coordinate.locationPointOfY, m.direction.string()
 }
 
 func (m *MarsRoverNew) sendCommand(commands []byte) (int, int, string) {
@@ -25,7 +49,7 @@ func (m *MarsRoverNew) sendCommand(commands []byte) (int, int, string) {
 			m.turnRight()
 		}
 	}
-	return m.coordinate.locationPointOfX, m.coordinate.locationPointOfY, m.direction.string()
+	return m.coordinate.locationPointOfX, m.coordinate.locationPointOfY, m.direction
 }
 
 func (m *MarsRoverNew) turnRight() {
@@ -53,6 +77,16 @@ func (m *MarsRoverNew) turnLeft() {
 }
 
 func (m *MarsRoverNew) moveForward() {
+	directionMoveCoordinateMap := map[string]Coordinate{
+		"E": NewCoordinate(1, 0),
+		"W": NewCoordinate(-1, 0),
+		"N": NewCoordinate(0, 1),
+		"S": NewCoordinate(0, -1),
+	}
+	m.coordinate.move(directionMoveCoordinateMap[m.direction])
+}
+
+func (m *MarsRover) moveForward() {
 	directionMoveCoordinateMap := map[string]Coordinate{
 		"E": NewCoordinate(1, 0),
 		"W": NewCoordinate(-1, 0),
